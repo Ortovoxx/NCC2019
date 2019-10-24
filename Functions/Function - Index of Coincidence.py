b8851ebfd0f39750b59f66b936de591632a5b6a0
Euan <56805259+Ortovoxx@users.noreply.github.com>
2019-10-24 22:05:02 +0100

    trying to get the IC to work

--- Functions/Function - Index of Coincidence.py
+++ Functions/Function - Index of Coincidence.py
@@ -3,16 +3,16 @@
 
 
 def ic(self):
-  num = 0.0
-  den = 0.0
-  for val in self.count.values():
-    i = val
-    num += i * (i - 1)
-    den += i
-  if (den == 0.0):
-    return 0.0
-  else:
-    return num / ( den * (den - 1))
+    num = 0.0
+    den = 0.0
+    for val in self.count.values():
+        i = val
+        num += i * (i - 1)
+        den += i
+    if (den == 0.0):
+        return 0.0
+    else:
+        return num / ( den * (den - 1))
 
 
 user = input("text: ")

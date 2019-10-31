98479df5851da2e0c2e38fa282f13017eb0c5f3f
Euan <56805259+Ortovoxx@users.noreply.github.com>
2019-10-31 14:49:20 +0000

    removed semicolon and replaced with spaces for all ngram files + added file loading to ngramFitness function

--- Functions/Function - ngramFitness.py
+++ Functions/Function - ngramFitness.py
@@ -1,7 +1,7 @@
 '''
 Allows scoring of text using n-gram probabilities
 17/07/12
-'''
+
 from math import log10
 
 # 389,373 unique quadrams in the file
@@ -10,7 +10,7 @@ from math import log10
 
 class ngram_score(object):
     def __init__(self,ngramfile,sep=' '):
-        ''' load a file containing ngrams and counts, calculate log probabilities '''
+        # load a file containing ngrams and counts, calculate log probabilities 
         self.ngrams = {}
         for line in file(ngramfile):
             key,count = line.split(sep) 
@@ -23,10 +23,39 @@ class ngram_score(object):
         self.floor = log10(0.01/self.N)
 
     def score(self,text):
-        ''' compute the score of text '''
+        # compute the score of text 
         score = 0
         ngrams = self.ngrams.__getitem__
         for i in xrange(len(text)-self.L+1):
             if text[i:i+self.L] in self.ngrams: score += ngrams(text[i:i+self.L])
             else: score += self.floor          
         return score
+
+'''
+import os
+
+
+
+
+
+
+os.chdir("/Users/Euan/Desktop/NCC2019/Cryptanalysis/ngrams")
+x = os.getcwd() #current working directory
+y = os.listdir() #lists all items in a directory
+
+
+#print(y)
+#print(x)
+
+quadramDitionary = {}
+index = 0
+with open("english_quadgrams.txt", "r") as f:
+    quadramData = f.read()
+    quadramArray = quadramData.split()
+    while index < len(quadramArray):
+        quadramDitionary[quadramArray[index]] = int(quadramArray[index + 1])
+        index = index + 2
+    #print(quadramDitionary)
+
+
+print("done")

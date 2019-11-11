657b47d0679a1d39f7f616c7795b44398bf0996a
Euan <56805259+Ortovoxx@users.noreply.github.com>
2019-11-11 21:42:26 +0000

    Added 1 and 2 word counts again... oops

--- Main/Transposition.py
+++ Main/Transposition.py
@@ -36,7 +36,7 @@ print(plaintextarray)
 plaintext = ''.join(str(x) for x in plaintextarray)
 
 print(plaintext)
-'''
+
 
 from pycipher import ColTrans
 import os
@@ -179,3 +179,65 @@ while t < 5000:
     print(x)
     print()
   t += 1
+  '''
+
+'''
+from word_score import word_score
+fitness = word_score()
+x = fitness.score('HELLOHOWAREYOUTODAY')
+print(x)
+'''
+from math import log10
+import os
+
+class word_score(object):
+    def __init__(self):
+        ''' load a file containing ngrams and counts, calculate log probabilities '''
+        os.chdir("/Users/Euan/Desktop/NCC2019/Cryptanalysis/word counts")
+        with open("Double word counts.txt") as file:
+            self.Pw = {}
+            for line in file('count_1w.txt'):
+                key,count = line.split('\t') 
+                self.Pw[key.upper()] = self.Pw.get(key.upper(), 0) + int(count)
+            self.N = 1024908267229 ## Number of tokens
+            #calculate first order log probabilities
+            for key in self.Pw.keys():
+                self.Pw[key] = log10(float(self.Pw[key])/self.N)
+            #get second order word model 
+            self.Pw2 = {}
+            for line in file('count_2w.txt'):
+                key,count = line.split('\t') 
+                self.Pw2[key.upper()] = self.Pw2.get(key.upper(), 0) + int(count)
+            #calculate second order log probabilities
+            for key in self.Pw2.keys():
+                word1,word2 = key.split()
+                if word1 not in self.Pw: 
+                    self.Pw2[key] = log10(float(self.Pw2[key])/self.N)
+                else: 
+                    self.Pw2[key] = log10(float(self.Pw2[key])/self.N) - self.Pw[word1]
+            # precalculate the probabilities we assign to words not in our dict, L is length of word
+            self.unseen = [log10(10./(self.N * 10**L)) for L in range(50)]        
+        
+    # conditional word probability    
+    def cPw(self,word,prev='<UNK>'):
+        if word not in self.Pw: 
+            return self.unseen[len(word)]
+        elif prev+' '+word not in self.Pw2: 
+            return self.Pw[word]
+        else: 
+            return self.Pw2[prev+' '+word]
+    
+    def score(self,text,maxwordlen=20):
+        prob = [[-99e99]*maxwordlen for _ in range(len(text))]
+        strs = [['']*maxwordlen for _ in range(len(text))]
+        for j in range(maxwordlen):
+            prob[0][j] = self.cPw(text[:j+1])
+            strs[0][j] = [text[:j+1]]
+        for i in range(1,len(text)):
+            for j in range(maxwordlen):
+                if i+j+1 > len(text): break
+                candidates = [(prob[i-k-1][k] + self.cPw(text[i:i+j+1],strs[i-k-1][k][-1]),
+                               strs[i-k-1][k] + [text[i:i+j+1]] ) for k in range(min(i,maxwordlen))]
+                prob[i][j], strs[i][j] = max(candidates)
+        ends = [(prob[-i-1][i],strs[-i-1][i]) for i in range(min(len(text),maxwordlen))]
+        return max(ends)
